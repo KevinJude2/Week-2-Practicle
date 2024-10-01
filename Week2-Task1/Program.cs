b8d77f5b81fa330c15e09c5b60c26db44efd6446
csf23sak <csf23sak@MF124PC56>
2024-10-01 09:33:26 +0100

    Add project files.

---
+++ Week2-Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+void main ()
+{
+}
+
+
+void PrintMenu()
+{
+
+
+    Console.Write("Please enter in a valid option from below: ");
+    Console.Write("1. Hello in French? ");
+    Console.Write("2. Hello in Spanish? ");
+    Console.Write("3. Hello in German? ");
+    Console.Write("4. Hello in Italian? ");
+    Console.Write("Exit application");
+
+}
+
+static String InputOption ()
+{
+    try
+    {
+        String option;
+        String choice = (Console.ReadLine());
+
+        case "1":
+            Con
+
+
+
+
+
+
+
+
+
