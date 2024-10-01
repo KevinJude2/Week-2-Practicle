52f5bf29078cc0d61960bf8cc8bdee5f52956e20
Kevin Saji Jude <Saji_Jude-K@ulster.ac.uk>
2024-10-01 11:19:11 +0100

    Implemented code for task 1

--- Week2-Task1/Program.cs
+++ Week2-Task1/Program.cs
@@ -1,35 +1,101 @@
 ﻿
 
-void main ()
+
+
+
+main();
+void main()
 {
+    PrintMenu();
+   
+
+
+
+    int num;
+    do
+    {
+        
+
+        Console.WriteLine("Enter a number (greater than 5 to quit): ");
+        num = Convert.ToInt32(Console.ReadLine());
+        InputOption();
+    } 
+    while (num < 5 );
+
+    
+
+    Console.WriteLine($"You entered: {num} which quit the program.");
+
 }
 
 
-void PrintMenu()
+static void PrintMenu()
 {
 
 
-    Console.Write("Please enter in a valid option from below: ");
-    Console.Write("1. Hello in French? ");
-    Console.Write("2. Hello in Spanish? ");
-    Console.Write("3. Hello in German? ");
-    Console.Write("4. Hello in Italian? ");
-    Console.Write("Exit application");
+    Console.WriteLine("Please enter in a valid option from below: ");
+    Console.WriteLine("1. Hello in French? ");
+    Console.WriteLine("2. Hello in Spanish? ");
+    Console.WriteLine("3. Hello in German? ");
+    Console.WriteLine("4. Hello in Italian? ");
+    Console.WriteLine("Exit application");
 
 }
-
-static String InputOption ()
+void InputOption()
 {
     try
     {
-        String option;
-        String choice = (Console.ReadLine());
+        Console.Write("Select your choice (1-5): ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        GetMessage(choice);
+    }
+    
+    catch (Exception ex)
+    {
 
-        case "1":
-            Con
+        Console.WriteLine($"Error:{ex.Message}. Please enter a valid option.");
+    }
+    finally
+    {
+       
+    }
+    
+}
 
+void GetMessage (int choice)
+{
 
+    
 
+    switch (choice)
+    {
+        case 0:
+            Console.Write("Goodbye");
+            break;
+
+        case 1:
+            Console.Write("Bonjour");
+            break;
+
+        case 2:
+            Console.Write("Ola");
+            break;
+
+        case 3:
+            Console.Write("Hallo");
+            break;
+
+        case 4:
+            Console.Write("CIao");
+            break;
+
+        default:
+            Console.Write("Please enter in a valid option");
+            break;
+
+    }
+
+}
 
 
 
